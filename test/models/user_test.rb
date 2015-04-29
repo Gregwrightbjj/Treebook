db47557693d6ccc148d3aaa44fee71bff1e605ad
Greg Wright <gregwrightbjj@gmail.com>
2015-04-29 16:12:19 -0400

    testing profile name

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -33,4 +33,12 @@ class UserTest < ActiveSupport::TestCase
   	assert !user.errors[:profile_name].empty?
   	assert user.errors[:profile_name].include?("Must be formatted correctly.")
   end
+   test "a user can have a correctly formated profile name"  do
+    user = User.new
+    user.password = user.password_confirmation = "asdfasdf"
+    user.profile_name = "monica_1"
+    
+    # assert user.valid?
+  end
+
 end
